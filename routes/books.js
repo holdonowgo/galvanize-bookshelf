a8514b90f6bf6325ba6b4cc3e5390a4136bdf9f4
Randall Spencer <randallspencer@gmail.com>
2017-03-07 20:19:57 -0800

    Pass books and books/:id routes in test

--- routes/books.js
+++ routes/books.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 'use strict';
 
 const express = require('express');
@@ -6,5 +8,39 @@ const express = require('express');
 const router = express.Router();
 
 // YOUR CODE HERE
+const knex = require('../knex.js');
+const humps = require('humps');
+
+router.route('/books')
+    .get((req, res) => {
+        //status code 200
+        //select * from drivers
+        knex('books')
+            .orderBy('title')
+            .then((books) => {
+                books = humps.camelizeKeys(books);
+                res.send(books);
+            })
+            .catch((err) => {
+                res.sendStatus(500);
+            });
+    });
+
+
+router.route('/books/:id')
+    .get((req, res) => {
+        //status code 200
+        //select * from drivers
+        knex('books')
+            .where('id', req.params.id)
+            .then((books) => {
+                let book = humps.camelizeKeys(books[0]);
+                res.status(200).json(book);
+            })
+            .catch((err) => {
+                res.sendStatus(500);
+            });
+    });
+
 
 module.exports = router;
