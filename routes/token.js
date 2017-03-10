6ba8c8a37b7b663aa6a84dc4f20efb9d31a78173
Randall Spencer <randallspencer@gmail.com>
2017-03-10 14:49:28 -0800

    Pass first test of part 4

--- routes/token.js
+++ routes/token.js
@@ -8,18 +8,64 @@ const express = require('express');
 const router = express.Router();
 
 // YOUR CODE HERE
+const bcrypt = require('bcrypt-as-promised');
 const knex = require('../knex.js');
 const humps = require('humps');
+const jwt = require('jsonwebtoken');
 
 router.route('/token')
-    .get((req, res) => {
-
-    })
-    .post((req, res) => {
-        let credentials = {
-            email: req.body.email,
-            password: req.body.password
+    .get((req, res, next) => {
+        console.log('enter the dragon');
+        if (!req.cookies.token) {
+            console.log(req.cookies);
+            console.log('exit the dragon');
+            return res.status(200).json(false);
         }
+        console.log('has token');
+        jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+            if (err) {
+                //unauthorized
+                console.log('error');
+                return res.status(200).json(false);
+            }
+            //the payload is the claim that we sent the client. In this case {userId}
+            //if it is present, the user is authorized. Do what you need to with userId
+            if (payload.userId) {
+                return res.status(200).json(true);
+            } else {
+                console.log('payload', payload);
+                return res.status(200).json(false);
+            }
+        });
+    })
+    .post((req, res, next) => {
+        knex('users').select('hashed_password').where('email', req.body.email)
+            .then((result) => {
+                // console.log('hashed_password', result[0].hashed_password);
+                let hashed_password = result[0].hashed_password;
+                return bcrypt.compare(req.body.password, hashed_password);
+            })
+            .then((is_match) => {
+                if (is_match) {
+                    const claim = {
+                        userId: user.id
+                    }; //this is our 'session'
+                    const token = jwt.sign(claim, process.env.JWT_KEY, { //use this environment variable to sign the cookie
+                        expiresIn: '7 days' // Adds an exp field to the payload
+                    });
+
+                    res.cookie('token', token, {
+                        httpOnly: true,
+                        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7), // 7 days
+                        secure: router.get('env') === 'production' // Set from the NODE_ENV
+                    });
+                } else {
+                    res.status(200).send('false');
+                }
+            })
+            .catch((err) => {
+                next(err);
+            });
     })
     .delete((req, res) => {
 
