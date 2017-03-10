e1b35aba5b4d586258e7832957215c779d1bf401
Randall Spencer <randallspencer@gmail.com>
2017-03-09 19:03:25 -0800

    Pass migrate seed test part 3

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
+/*jshint esversion: 6 */
 'use strict';
 
 const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const bcrypt = require('bcrypt-as-promised');
 
 // YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12)
+    .then((hashed_password) => {
+      console.log(req.body.email, hashed_password);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
